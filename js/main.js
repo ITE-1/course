bf0d87a649616b4aa1660e9fee7845e2160d7653
Alex <devilout123@gmail.com>
2023-02-22 09:35:26 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+// console.log(persone);
+// let arr = ['plum.jpg', 'orange.jpg', 3, 'applejuice.jpg'];
+// console.log(arr[1]);
+
+// const arrObj = {
+//     a: 10,
+//     b: 20,
+// };
+// console.log(arrObj);
+// arrObj.c = '1234';
+// console.log(arrObj['c']);
+//alert('privet');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+//Урок12 (интерполяция)
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+// console.log('arr' + +"5");
+//Урок13 (операторы)
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr); 
+// const isCheked = true,
+//       isClose = true;
+// console.log(isCheked || !isClose);
+// function newContent() {
+//     document.open();
+//     document.write("<h1>Долой старое, хотим нове</h1>");
+//     document.close();
+// }
+let a = 5,
+    b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
